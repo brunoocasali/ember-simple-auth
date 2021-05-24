d6169d1fa302eb3311bd5103e23f4acf9d8cd715
Bruno Casali <brunoocasali@gmail.com>
2021-05-24 19:29:14 -0300

    Remove register-version.js (TEMP)

--- packages/ember-simple-auth/index.js
+++ packages/ember-simple-auth/index.js
@@ -3,11 +3,8 @@
 /* eslint-env node */
 /* eslint-disable no-var, object-shorthand, prefer-template */
 
-var writeFile = require('broccoli-file-creator');
-var version = require('./package.json').version;
 var path = require('path');
 var Funnel = require('broccoli-funnel');
-var MergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-simple-auth',
@@ -16,21 +13,15 @@ module.exports = {
     this._super.included.apply(this, arguments);
     this._ensureThisImport();
 
-    this.import('vendor/ember-simple-auth/register-version.js');
     this.import('vendor/base64.js');
   },
 
   treeForVendor() {
-    var content = "Ember.libraries.register('Ember Simple Auth', '" + version + "');";
-    var registerVersionTree = writeFile(
-      'ember-simple-auth/register-version.js',
-      content
-    );
     var base64Tree = new Funnel(path.dirname(require.resolve('base-64')), {
       files: ['base64.js']
     });
 
-    return MergeTrees([registerVersionTree, base64Tree]);
+    return base64Tree;
   },
 
   _ensureThisImport: function() {
